bff508092b1e79b34f75ee89b9645bb9a8036c0a
ngochoa203 <hongochoa203@gmail.com>
2025-06-30 06:21:31 +0700

    Scrum-18: Fix font

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Montserrat', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        sans: ['Montserrat'],
       },
     },
   },
