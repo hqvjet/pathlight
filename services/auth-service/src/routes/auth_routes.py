399724b3a6adf489de3486fda81bb6fc00dbe050
ngochoa203 <hongochoa203@gmail.com>
2025-07-05 01:17:27 +0700

    Improve-auth-sv: Rename api

--- services/auth-service/src/routes/auth_routes.py
+++ services/auth-service/src/routes/auth_routes.py
@@ -6,7 +6,7 @@ from ..database import get_db
 from ..schemas.auth_schemas import *
 from ..controllers.auth_controller import *
 
-router = APIRouter(prefix="/api/v1/auth", tags=["Authentication"])
+router = APIRouter(prefix="", tags=["Authentication"])
 
 # 1.1. Đăng ký
 @router.post("/signup", response_model=MessageResponse)
